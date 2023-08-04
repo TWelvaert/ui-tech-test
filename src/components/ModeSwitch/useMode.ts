4c5988f9c1342e866bed507c7db992b297ebca2b
TWelvaert <thomaswelvaert@hotmail.com>
2023-08-04 10:14:46 +0200

    add edit and preview mode

--- src/components/ModeSwitch/useMode.ts
+++ src/components/ModeSwitch/useMode.ts
@@ -1,7 +1,35 @@
-import { useReducer } from "react"
-type Mode = "preview" | "edit"
+import { useReducer, useEffect } from "react";
 
-export default function useMode() {
-    // TODO: re-implement this hook so that it reads and writes the mode query parameter from the URL
-    return useReducer((mode: Mode) => (mode === "preview" ? "edit" : "preview"), "preview")
+type Mode = "preview" | "edit";
+
+function getInitialModeFromURL(): Mode {
+  const params = new URLSearchParams(window.location.search);
+  const mode = params.get("mode");
+  return mode === "edit" ? "edit" : "preview";
 }
+
+function updateURL(mode: Mode) {
+  const params = new URLSearchParams(window.location.search);
+  params.set("mode", mode);
+  const newURL = `${window.location.pathname}?${params}`;
+  window.history.replaceState({}, "", newURL);
+}
+
+function useMode() {
+  const [mode, dispatch] = useReducer(
+    (state: Mode, action: Mode) => action,
+    getInitialModeFromURL()
+  );
+
+  useEffect(() => {
+    updateURL(mode);
+  }, [mode]);
+
+  const toggleMode = () => {
+    dispatch(mode === "edit" ? "preview" : "edit");
+  };
+
+  return [mode, toggleMode];
+}
+
+export default useMode;
